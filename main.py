27da26d5a109e5762b8dbaa6e61c446012cb66fe
yashagrawal06 <yshagrawal1@gmail.com>
2018-11-22 16:52:01 -0800

    convert to int added

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ import src.train as train
 def main():
 	filename = 'data/sonar.all-data.csv'
 	dataset = train.load_csv(filename)
-	print (dataset)
+
+#Convert string to float
+	for i in range(0,len(dataset[0]) - 1):
+		a = train.str_column_to_float(dataset,i)
+
+#Convert class column to integers
+	b = train.str_column_to_integer(dataset, len(dataset[0]) -1 )
+	print(b)
+
 main()
