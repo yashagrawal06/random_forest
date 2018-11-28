ce540f153bdc6d89a7cb12ae966eca0369eb5026
yashagrawal06 <yshagrawal1@gmail.com>
2018-11-27 18:19:32 -0800

    Test code

--- main.py
+++ main.py
@@ -2,46 +2,44 @@ from random import seed
 from random import randrange
 from csv import reader
 from math import sqrt
-import src.train as train
+from rf_testing import *
 
 def main():
-	filename = 'data/sonar.all-data.csv'
-	dataset = train.load_csv(filename)
-
-#Convert string to float
-	for i in range(0,len(dataset[0]) - 1):
-		train.str_column_to_float(dataset,i)
-
-#Convert str to int
-	train.str_column_to_int(dataset, len(dataset[0])-1)
-
-#Make prediction with list of bagged trees
-	def bagging_predictions(trees, row):
+	# Make a prediction with a list of bagged trees
+	def bagging_predict(trees, row):
 		predictions = [predict(tree, row) for tree in trees]
 		return max(set(predictions), key=predictions.count)
 
-#Random Forest algorithm
-	def random_forest(train, test, max_depth, min_size, sample_size, n_trees,
-	n_features):
+	# Random Forest Algorithm
+	def random_forest(train, test, max_depth, min_size, sample_size, n_trees, n_features):
 		trees = list()
 		for i in range(n_trees):
-			sample = train.subsample(train, sample_size)
+			sample = subsample(train, sample_size)
 			tree = build_tree(sample, max_depth, min_size, n_features)
 			trees.append(tree)
-		predictions = [bagging_predictions(trees, row) for row in test]
+		predictions = [bagging_predict(trees, row) for row in test]
 		return(predictions)
 
-#Test the random forest algorithm
-	k_folds = 5
+	# Test the random forest algorithm
+	seed(2)
+	# load and prepare data
+	filename = 'data/dt_test.csv'
+	dataset = load_csv(filename)
+	# convert string attributes to integers
+	for i in range(0, len(dataset[0])-1):
+		str_column_to_float(dataset, i)
+	# convert class column to integers
+	str_column_to_int(dataset, len(dataset[0])-1)
+	# evaluate algorithm
+	n_folds = 5
 	max_depth = 10
 	min_size = 1
 	sample_size = 1.0
 	n_features = int(sqrt(len(dataset[0])-1))
-	for n_trees in [1, 5, 10]:
-		scores = train.evaluate_algorithm(dataset, random_forest, k_folds,
-		max_depth, min_size, sample_size, n_trees, n_features)
+	for n_trees in [10, 25, 50]:
+		scores = evaluate_algorithm(dataset, random_forest, n_folds, max_depth, min_size, sample_size, n_trees, n_features)
 		print('Trees: %d' % n_trees)
 		print('Scores: %s' % scores)
-		print('Mean accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
+		print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
 
 main()
--- rf_testing.py
+++ rf_testing.py
@@ -1,14 +1,13 @@
-#Importing the libraries
-
+# Random Forest Algorithm on Sonar Dataset
 from random import seed
 from random import randrange
 from csv import reader
 from math import sqrt
 
-#Load the CSV file
+# Load a CSV file
 def load_csv(filename):
 	dataset = list()
-	with open(filename,  "r") as file:
+	with open(filename, 'r') as file:
 		csv_reader = reader(file)
 		for row in csv_reader:
 			if not row:
@@ -16,13 +15,12 @@ def load_csv(filename):
 			dataset.append(row)
 	return dataset
 
-
-#Convert string column to float
+# Convert string column to float
 def str_column_to_float(dataset, column):
 	for row in dataset:
 		row[column] = float(row[column].strip())
 
-#Convert string column to integer
+# Convert string column to integer
 def str_column_to_int(dataset, column):
 	class_values = [row[column] for row in dataset]
 	unique = set(class_values)
@@ -33,40 +31,30 @@ def str_column_to_int(dataset, column):
 		row[column] = lookup[row[column]]
 	return lookup
 
-#Cross-Validation
-def cross_validation_split(dataset,k_folds):
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
 	dataset_split = list()
 	dataset_copy = list(dataset)
-	fold_size = int(len(dataset)/k_folds)
-	for i in range(k_folds):
+	fold_size = int(len(dataset)/n_folds)
+	for i in range(n_folds):
 		fold = list()
-		while len(fold)<fold_size:
+		while len(fold) < fold_size:
 			index = randrange(len(dataset_copy))
 			fold.append(dataset_copy.pop(index))
 		dataset_split.append(fold)
 	return dataset_split
 
-#Split dataset based on attribute and attribute value
-def test_split(index, value, dataset):
-	left, right = list(), list()
-	for row in dataset():
-		if row[index] < value:
-			left.append(row)
-		else:
-			right.append(row)
-	return left, right
-
-#Calculate accuracy
-def metric_accuracy(actual, predicted):
-	correct_pred = 0
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+	correct = 0
 	for i in range(len(actual)):
 		if actual[i] == predicted[i]:
-			correct_pred +=1
-	return correct_pred / float(len(actual)) * 100.0
+			correct += 1
+	return correct / float(len(actual)) * 100.0
 
-#Evaluate algorith with cross_validation_split
-def evaluate_algorithm(dataset, algorithm, k_folds, *args):
-	folds = cross_validation_split(dataset, k_folds)
+# Evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+	folds = cross_validation_split(dataset, n_folds)
 	scores = list()
 	for fold in folds:
 		train_set = list(folds)
@@ -79,26 +67,43 @@ def evaluate_algorithm(dataset, algorithm, k_folds, *args):
 			row_copy[-1] = None
 		predicted = algorithm(train_set, test_set, *args)
 		actual = [row[-1] for row in fold]
-		accuracy = metric_accuracy(actual, predicted)
+		accuracy = accuracy_metric(actual, predicted)
 		scores.append(accuracy)
 	return scores
 
-#Calculate Gini Index for Split
-def gini_index(groups, class_values):
+# Split a dataset based on an attribute and an attribute value
+def test_split(index, value, dataset):
+	left, right = list(), list()
+	for row in dataset:
+		if row[index] < value:
+			left.append(row)
+		else:
+			right.append(row)
+	return left, right
+
+# Calculate the Gini index for a split dataset
+def gini_index(groups, classes):
+	# count all samples at split point
+	n_instances = float(sum([len(group) for group in groups]))
+	# sum weighted Gini index for each group
 	gini = 0.0
-	for class_value in class_values:
-		for group in groups:
-			size = len(group)
-			if size ==0:
-				continue
-			proportion = [row[-1] for row in group].count(class_value)
-			float(size)
-			gini += (proportion * (1.0-proportion))
+	for group in groups:
+		size = float(len(group))
+		# avoid divide by zero
+		if size == 0:
+			continue
+		score = 0.0
+		# score the group based on the score for each class
+		for class_val in classes:
+			p = [row[-1] for row in group].count(class_val) / size
+			score += p * p
+		# weight the group score by its relative size
+		gini += (1.0 - score) * (size / n_instances)
 	return gini
 
-#Select best split point for dataset
+# Select the best split point for a dataset
 def get_split(dataset, n_features):
-	class_values = list(row[-1] for row in dataset)
+	class_values = list(set(row[-1] for row in dataset))
 	b_index, b_value, b_score, b_groups = 999, 999, 999, None
 	features = list()
 	while len(features) < n_features:
@@ -108,56 +113,50 @@ def get_split(dataset, n_features):
 	for index in features:
 		for row in dataset:
 			groups = test_split(index, row[index], dataset)
-			gini = gini_index(groups, class_value)
+			gini = gini_index(groups, class_values)
 			if gini < b_score:
-				b_index, b_value, b_score, b_groups = index, row[index], gini,
-				groups
-	return{'index': b_index, 'value':b_value, 'groups':b_groups}
+				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+	return {'index':b_index, 'value':b_value, 'groups':b_groups}
 
-#Create a terminal node value
+# Create a terminal node value
 def to_terminal(group):
-	outcomes = [row[-1] for row in dataset]
+	outcomes = [row[-1] for row in group]
 	return max(set(outcomes), key=outcomes.count)
 
-#Create Child Splits or terminal/leaf node
+# Create child splits for a node or make terminal
 def split(node, max_depth, min_size, n_features, depth):
 	left, right = node['groups']
 	del(node['groups'])
+	# check for a no split
 	if not left or not right:
-		node['left'] = node['right'] = to_terminal(left+right)
+		node['left'] = node['right'] = to_terminal(left + right)
 		return
+	# check for max depth
 	if depth >= max_depth:
 		node['left'], node['right'] = to_terminal(left), to_terminal(right)
 		return
+	# process left child
 	if len(left) <= min_size:
 		node['left'] = to_terminal(left)
 	else:
 		node['left'] = get_split(left, n_features)
 		split(node['left'], max_depth, min_size, n_features, depth+1)
+	# process right child
 	if len(right) <= min_size:
 		node['right'] = to_terminal(right)
 	else:
 		node['right'] = get_split(right, n_features)
-		spilt(node['right'], max_depth, min_size, n_features, depth+1)
+		split(node['right'], max_depth, min_size, n_features, depth+1)
 
-#Build a Decision Tree
+# Build a decision tree
 def build_tree(train, max_depth, min_size, n_features):
 	root = get_split(train, n_features)
 	split(root, max_depth, min_size, n_features, 1)
 	return root
 
-def subsample(dataset, ratio):
-	sample = list()
-	n_sample = round(len(dataset) * ratio)
-	while len(sample) < n_sample:
-		index = randrange(len(dataset))
-		sample.append(dataset[index])
-	return sample
-
-
-#Predictions with Decision Tree
+# Make a prediction with a decision tree
 def predict(node, row):
-	if row(node['index']) < node['value']:
+	if row[node['index']] < node['value']:
 		if isinstance(node['left'], dict):
 			return predict(node['left'], row)
 		else:
@@ -167,3 +166,12 @@ def predict(node, row):
 			return predict(node['right'], row)
 		else:
 			return node['right']
+
+# Create a random subsample from the dataset with replacement
+def subsample(dataset, ratio):
+	sample = list()
+	n_sample = round(len(dataset) * ratio)
+	while len(sample) < n_sample:
+		index = randrange(len(dataset))
+		sample.append(dataset[index])
+	return sample
